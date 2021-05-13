fc1cabda9e037e27c5ebc33e41b42eaafe293c28
milanjokanovic <milan.jokanovic98@gmail.com>
2021-05-13 13:37:37 +0200

    inital spring api

---
+++ book_recommender/src/main/java/rs/sbnz/book_recommender/BookRecommenderApplication.java
@@ -0,0 +1,13 @@
+package rs.sbnz.book_recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRecommenderApplication.class, args);
+	}
+
+}
