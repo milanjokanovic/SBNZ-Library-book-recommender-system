03d3b4446634a3f76156840ac16a839cf337c101
senpai132 <acanikolic021@gmail.com>
2021-05-17 13:15:43 +0200

    Author and Genre Crud

--- book_recommender/src/main/java/rs/sbnz/book_recommender/repositories/GenreRepository.java
+++ book_recommender/src/main/java/rs/sbnz/book_recommender/repositories/GenreRepository.java
@@ -1,4 +1,10 @@
 package rs.sbnz.book_recommender.repositories;
 
-public class GenreRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rs.sbnz.book_recommender.model.Genre;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Integer> {
+    Genre findByName(String name);
 }
