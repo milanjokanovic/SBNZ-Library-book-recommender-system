fa84b958be99aa8034ff4ace380285e937da2254
milanjokanovic <milan.jokanovic98@gmail.com>
2021-06-17 12:20:13 +0200

    fixed adding new books

--- book_recommender/src/main/java/rs/sbnz/book_recommender/repositories/GenreRepository.java
+++ book_recommender/src/main/java/rs/sbnz/book_recommender/repositories/GenreRepository.java
@@ -7,11 +7,12 @@ import org.springframework.stereotype.Repository;
 import rs.sbnz.book_recommender.model.Genre;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Integer> {
     Genre findByName(String name);
 
     @Query("SELECT genre FROM Genre genre WHERE genre.name IN (:names)")
-    List<Genre> findByGenreNames(@Param("names") List<String> names);
+    Set<Genre> findByGenreNames(@Param("names") List<String> names);
 }
